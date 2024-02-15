1b1bc8cef2a534b13b545abc288b973d0f1ca6b2
Samuel-okongo <so274@njit.edu>
2024-02-14 22:53:56 -0500

    finished the update

--- calculator/calulation.py
+++
@@ -1,6 +0,0 @@
-class Calculation:
-    def __init__(self, operation: str, operand1: float, operand2: float, result: float):
-        self.operation = operation
-        self.operand1 = operand1
-        self.operand2 = operand2
-        self.result = result
