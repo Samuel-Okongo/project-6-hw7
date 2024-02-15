eed65230e55cd764ed2da090edcf7dcd1d2a3785
Samuel-okongo <so274@njit.edu>
2024-02-14 21:43:40 -0500

    added the modifications

---
+++ calculator/calulation.py
@@ -0,0 +1,6 @@
+class Calculation:
+    def __init__(self, operation: str, operand1: float, operand2: float, result: float):
+        self.operation = operation
+        self.operand1 = operand1
+        self.operand2 = operand2
+        self.result = result
