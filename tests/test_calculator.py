004ad8ecb646f70a73b64b379aa753fb0d14cb1b
Samuel-okongo <so274@njit.edu>
2024-02-14 23:14:10 -0500

    added test_calcu

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,18 @@
-'''My Calculator Test'''
+import pytest
+from calculator.calculator import Calculator
 
-from calculator import add, subtract
+def test_add():
+    assert Calculator.add(1, 2) == 3
 
-def test_addition():
-    """
-    Tests the add function from the calculator module.
-    
-    Ensures that the addition of two numbers returns the correct sum.
-    """
-    assert add(2, 2) == 4
+def test_subtract():
+    assert Calculator.subtract(5, 3) == 2
 
-def test_subtraction():
-    """
-    Tests the subtract function from the calculator module.
-    
-    Ensures that subtracting the second number from the first returns the correct difference.
-    """
-    assert subtract(2, 2) == 0
+def test_multiply():
+    assert Calculator.multiply(2, 3) == 6
+
+def test_divide():
+    assert Calculator.divide(6, 2) == 3
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        Calculator.divide(1, 0)
